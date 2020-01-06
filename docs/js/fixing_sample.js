64310e07a35eb2def4aa7460a6afe6d47dd3083a
Yuta Takayama <yprogdbcs@icloud.com>
2020-01-06 19:02:49 +0900

    初版作成

---
+++ docs/js/fixing_sample.js
@@ -0,0 +1,10 @@
+$(function () {
+    revisePadding();
+    $(window).on("resize", revisePadding);
+
+    function revisePadding() {
+        $(".background_img").css({
+            "padding-top": $("header")[0].offsetHeight + "px",
+        });
+    }
+});
